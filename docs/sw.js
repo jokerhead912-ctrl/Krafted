d228e3ec39a3ee2f06a4291e3fe815b2e762b53a
Joker Head <jokerhead912-ctrl@users.noreply.github.com>
2026-07-23 11:30:08 +0800

    v6.6.4: will-change+contain:layout on text resize for 30+ image perf

--- docs/sw.js
+++ docs/sw.js
@@ -1,8 +1,8 @@
 // Krafted v6.1.20 Service Worker
 // Standalone file for PWA caching on GitHub Pages.
 
-const CACHE_NAME = 'krafted-v6.6.3-' + Date.now();
-const APP_VERSION = '6.6.3';
+const CACHE_NAME = 'krafted-v6.6.4-' + Date.now();
+const APP_VERSION = '6.6.4';
 
 const PRE_CACHE = ['./'];
 
